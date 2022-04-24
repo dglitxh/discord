c8191631b78c668f1dff78b2f0617b9f1e009fdd
YD <ydglitch@gmail.com>
2022-04-24 16:42:05 +0000

    feature: commands

--- bot.py
+++ bot.py
@@ -1,23 +1,59 @@
+import json
 import discord 
 import os
+import requests
 from dotenv import load_dotenv
+from discord.ext import commands
+import random
 
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 load_dotenv()
-bot_client = discord.Client()
+TOKEN = os.getenv('BOT_TOKEN')
+GUILD = os.getenv('GUILD')
 
-@bot_client.event
+
+def get_quote():
+    print("searching........")
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)[0]
+    return (f"{json_data['q']}\n  ~ {json_data['a']}")
+
+
+@bot.event
 async def on_ready():
-    print("Yo we logged in as {0.user}".format(bot_client))
+    guild = discord.utils.find(lambda guild: guild.name == GUILD, bot.guilds)
 
-@bot_client.event
-async def on_message(message):
-    if message.author == bot_client.user:
-        return
+    print("Yo we logged in as {0.user}".format(bot))
+    print(f'{guild.name}(id: {guild.id})')
 
-    if message.content.startswith('hello' or 'Hello'):
-        await message.channel.send('Hello friend!!')
+    print(f'Guild Members:')
+    async for member in guild.fetch_members(limit=150):
+        print(f" -{member.name}")
+    
+@bot.command(name="motivate", help="-- Responds with a motivational  quote")
+async def motivator(ctx):
+    quote = get_quote()
+    print(quote)
+    await ctx.send(quote)
 
 
 
-bot_client.run(os.getenv('BOT_TOKEN'))
+@bot.command(name="flip-coin", help=" -- Flips a coin")
+async def flip(ctx):
+    coin_face = random.choice(["Head", "Tail"])
+    await ctx.send(coin_face)
+
+# @bot.event
+# async def on_message(message):
+#     if message.author == bot.user:
+#         return
+
+#     if message.content.startswith('hello'):
+#         await message.channel.send('Hello friend!!')
+
+
+bot.run(TOKEN)
