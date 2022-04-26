3983f751d721ad1a32625b87fbabed1b1443e169
YD <ydglitch@gmail.com>
2022-04-26 08:05:43 +0000

    create channel

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import json
+from sys import prefix
 import discord 
 import os
 import requests
@@ -53,6 +54,21 @@ async def on_member_join(member):
     await member.dm_channel.send(
         f"Hello {member.name}, welcome to the XEVR server"
     )
+
+@bot.command(prefix="create-channel" help=" -- Create new channel with name")
+@commands.has_role("admin")
+async def create_channel(ctx, channel_name="XEVR"):
+    guild = ctx.guild
+    existing_channel = discord.utils.get(guild.channels, name=channel_name)
+    if not existing_channel:
+        print(f'Creating a new channel: {channel_name}')
+        await guild.create_text_channel(channel_name)
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        await ctx.send('You do not have the correct role for this command.')
+
 # @bot.event
 # async def on_message(message):
 #     if message.author == bot.user:
